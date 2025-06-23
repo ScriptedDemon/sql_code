9ca6427779356d49db3cfa6c9f366f0e1b23214a
ScriptedDemon <155841199+ScriptedDemon@users.noreply.github.com>
2025-06-23 13:39:15 +0530

    Update master_view.sql

--- master_view.sql
+++ master_view.sql
@@ -1,16 +1,46 @@
-DROP VIEW IF EXISTS master_view_1;
+DROP VIEW IF EXISTS master_view;
 
-CREATE VIEW master_view_1 AS
+CREATE VIEW master_view AS
 SELECT
-    h.`new_hsnumber` AS hs_id
+    h.`new_hsnumber` AS hs_id,
+    h.`hazard`,
+    h.`hazardous_situation`,
+    h.`harm`,
+    h.`core_infusion_/_large_volume_pump`,
+    h.`syringe_pump`,
+    h.`pain_pumps`,
+    h.`therapeutic_drug`,
+    h.`compounding`,
+    h.`parenteral_nutrition`,
+    h.`critical`,
+    h.`moderate`,
+    h.`minor`,
+    h.`criticality_ranking`,
+    -- Appendix b
+    
+    b.`critical_semi_quantitative`,
+    b.`moderate_semi_quantitative`,
+    b.`minor_semi_quantitative`,
+    c.`p1_predicted_by_subject_matter_expertise`,
+    c.`p1`,
+    b.`critical`,
+    b.`moderate`,
+    b.`minor`,
+    b.`critical.1`,
+    b.`moderate.1`,
+    b.`minor.1`,
+    c.`data_summary_derived_p1`,
+    c.`ucl`,
+    c.`ucl_value_for_each_plants`
 
-FROM `hsha-parenteral-infusion` h
+FROM
+    `hsha-parenteral-infusion` h
 
-LEFT JOIN `appendix c - p1` p1
-    ON h.`new_hsnumber` = p1.hs_id
+LEFT JOIN `appendix c - p1` c
+    ON h.`new_hsnumber` = c.`hs_id`
 
-LEFT JOIN `appendix_b_p2_conversion` p2
-    ON h.`new_hsnumber` = p2.hazardouz_situation_id;
+LEFT JOIN  `appendix_b_p2_conversion` b
+    ON h.`new_hsnumber` = b.`hazardous_situation_id`;
 
 
 
